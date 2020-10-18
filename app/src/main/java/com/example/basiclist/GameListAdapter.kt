ad4680c6e1bfad421f57e4a3ca8f78483c20727a
riyaz <riyazpasha880@gmail.com>
2020-10-18 14:58:51 +0530

    moved binding logic and inflatinng view logic to view holder

--- app/src/main/java/com/example/basiclist/GameListAdapter.kt
+++ app/src/main/java/com/example/basiclist/GameListAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.basiclist
 
-import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,24 +10,33 @@ class GameListAdapter(private val data: List<Game>) :
     RecyclerView.Adapter<GameListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.custom_list, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val game = data[position]
-        val res = holder.itemView.context.resources
-        holder.title.text = game.title
-        holder.description.text = game.description
+        holder.bind(game)
     }
+
 
     override fun getItemCount() = data.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.title)
-        val description: TextView = itemView.findViewById(R.id.description)
+        private val title: TextView = itemView.findViewById(R.id.title)
+        private val description: TextView = itemView.findViewById(R.id.description)
+
+        fun bind(game: Game) {
+            title.text = game.title
+            description.text = game.description
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.custom_list, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 }
